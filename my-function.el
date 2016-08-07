02a720a8421734d9829c5cb103f503635b1412b1
dinga <dingaking@gmail.com>
2016-08-07 21:14:24 +0900

    select current line.

--- my-function.el
+++ my-function.el
@@ -47,3 +47,39 @@
       (tiger 'fierce))
   (message "One kind of animal has %s and another is %s."
            zebra tiger))
+
+;; uninitialized variables in a let statement
+(let ((birch 3)
+      pine
+      fir
+      (oak 'some))
+  (message
+   "Here are %d variables with %s, %s, and %s value."
+   birch pine fir oak))
+
+;; 
+(if (> 5 4)                             ; if-part
+    (message "5 is greater than 4!")
+  (message "else"))   ; then-part
+
+
+(defun type-of-animal (characteristic)
+  "Print message in echo area depending on CHARACTERISTIC.
+If the CHARACTERISTIC is the symbol `fierce',
+then warn of a tiger."
+  (if (equal characteristic 'fierce)
+      (message "It's a tiger!")
+    (message "It's not a tiger!")))
+
+(type-of-animal 'fierce)
+(type-of-animal 'stripes)
+(type-of-animal 'zebra)
+
+
+(transient-mark-mode 1)
+(defun select-current-line ()
+  "Select the current line"
+  (interactive)
+  (end-of-line)
+  (set-mark (line-beginning-position)))
+
